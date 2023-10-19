3225b76df1d9c3eb148c784574622b4525149919
THMinh <t.h.minh101002@gmail.com>
2023-10-19 20:45:06 +0700

    createOrderDao, OrderRepository

---
+++ LaundryMidlePlatform/Pages/Orders/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObjects.Models;
+
+namespace LaundryMidlePlatform.Pages.Orders
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly BusinessObjects.Models.LaundryMiddlePlatformContext _context;
+
+        public DetailsModel(BusinessObjects.Models.LaundryMiddlePlatformContext context)
+        {
+            _context = context;
+        }
+
+      public Order Order { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.FirstOrDefaultAsync(m => m.OrderId == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Order = order;
+            }
+            return Page();
+        }
+    }
+}
---
+++ Repository/IRepository/IOrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using BusinessObjects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.IRepository
+{
+    public interface IOrderRepository
+    {
+        public IList<Order> findAllOrders();
+
+        public Order findOrderById(int orderId);
+
+        public bool updateOrder(Order order, int orderId);
+        public bool createOrder(Order order);
+        public bool deleteOrder(int orderId);
+    }
+}
