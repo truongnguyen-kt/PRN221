b9d454fa7b29c1b93f1a4950ad7d9e75a3a9fccb
nguyen phu <100401804+PhuThanh001@users.noreply.github.com>
2023-10-20 08:19:22 +0700

    Update UserDAO,RoleDAO,Repo

---
+++ Repository/Implement/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using BusinessObjects.Models;
+using DataAccess;
+using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Implement
+{
+    public class UserRepository : IUserRepository
+    {
+        public void AddNewCustomer(User newCustomer)
+        {
+            UserDAO.Instance.AddUser(newCustomer);
+        }
+
+        public void DeleteUser(int userId)
+        {
+            UserDAO.Instance.DeleteUser(userId);
+        }
+
+        public List<User?> GetAllUsers()
+        {
+            return UserDAO.Instance.GetAllUser();
+        }
+
+        public List<User?> GetUserByEmail(string email)
+        {
+           return UserDAO.Instance.GetCustomerByEmail(email);
+        }
+
+        public User GetUserById(int id)
+        {
+            return UserDAO.Instance.GetCustomerById(id);
+        }
+
+        public void UpdateUser(User user)
+        {           
+            UserDAO.Instance.UpdateUser(user);
+        }
+    }
+}
