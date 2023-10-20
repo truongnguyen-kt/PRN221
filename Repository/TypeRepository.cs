43d1a003a5bb1f34e9738b8eb46bd6f9b08be93b
MSI\Blues <truongnhlse160191@fpt.edu.vn>
2023-10-20 15:51:04 +0700

    push code

---
+++ Repository/TypeRepository.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class TypeRepository
+    {
+        public IList<BusinessObjects.Models.Type> findAllTypes() => TypeDAO.Instance.findAllTypes();
+
+        public BusinessObjects.Models.Type findTypeById(int orderId) => TypeDAO.Instance.findTypeById(orderId);
+
+        public bool updateType(BusinessObjects.Models.Type order, int orderId) => TypeDAO.Instance.updateType(order, orderId);
+        public bool createType(BusinessObjects.Models.Type order) => TypeDAO.Instance.createType(order);
+        public bool deleteType(int orderId) => TypeDAO.Instance.deleteType(orderId);
+    }
+}
